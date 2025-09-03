9ad6c94a9d046b7ee308fd688e055be6f0f81a6f
MariaEduardaLeles <maria.eleccel@aluno.edu.es.gov.br>
2025-09-03 11:56:23 -0300

    Create index.php

---
+++ aula 01- 3 Trimestre/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title> Aula 01 PHP </title>
+    </head>
+    <body>
+        <?php
+            echo " Hello World! ";
+            echo "Manu de jesus e Maria Leles";
+        ?>
+    </body>
+</html>
