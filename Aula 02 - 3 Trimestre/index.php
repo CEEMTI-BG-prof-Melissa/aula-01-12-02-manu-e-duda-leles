489b7376ac0c49e50555763f1aebfbcf2beddbf3
MariaEduardaLeles <maria.eleccel@aluno.edu.es.gov.br>
2025-09-03 11:58:21 -0300

    Create index.php

---
+++ Aula 02 - 3 Trimestre/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title> Aula 02/php </title>
+    </head>
+    <body>
+        <?php
+            $nome1="Manu de Jesus";
+            $nome2="Maria Leles";
+
+            echo "Meu nome é ".$nome1 ."e minha dupla é ".$nome2;
+        ?>
+    </body>
+</html>
